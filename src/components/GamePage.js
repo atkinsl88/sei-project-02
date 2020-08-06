e1ad5ec356084c527fec7f323a894577be2df0a4
atkinsl88 <liamatkins24@gmail.com>
2020-08-06 19:55:21 +0100

    workign version, just the last bits to do

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,63 +1,88 @@
 import React from 'react'
 import { Link  } from 'react-router-dom'
-import { getSingleTrumpQuote, getSingleKanyeQuote, getSingleQuote, randomLine } from '../lib/api'
+import { getSingleQuote } from '../lib/api'
+import image from '../assets/kanye-trump.png'
 
 class GamePage extends React.Component {
 
   state = {
-    trump: [],
-    kanye: [],
-    random: []
+    trump: null,
+    kanye: null,
+    totalGuesses: 0,
+    correctGuesses: 0,
+    isCorrectTrump: false,
+    isCorrectKanye: false,
+    score: null
   }
 
   async componentDidMount() {
     try {
-
-      const res = await getSingleTrumpQuote()
-      console.log(res.data.value)
-      this.setState({ trump: res.data.value })
-
-      const resKanye = await getSingleKanyeQuote()
-      console.log(resKanye.data.quote)
-      this.setState({ kanye: resKanye.data.quote })
-
-      // const resRandom = await getSingleQuote()
-      // console.log(resRandom)
-      // console.log(resRandom.data)
-      // console.log(resRandom.data.randomLine(line))
-
-
-      // const isTrump = this.handleRandom()
-      // console.log(isTrump)
-      // // const res = await getSingleKanyeQuote()
-      // // console.log(res.data.quote)
-      // const res = await isTrump ? getSingleTrumpQuote() : getSingleKanyeQuote() 
-      // this.setState({ quote: res.data.quote })
-      // console.log(res)
-
+      const resRandom = await getSingleQuote()
+      console.log(resRandom.data)
+      this.setState({ trump: resRandom.data.message, kanye: resRandom.data.quote })
     } catch (err) {
       console.log(err)
     }
   }
 
+  handleGuess = (e) => {
+    console.log(this.endGame())
+    if (!this.endGame()) {
+      const { trump, correctGuesses, totalGuesses } = this.state
+      const answer = trump ? 'trump' : 'kanye'
+      console.log(this.state)
+      this.setState({ 
+        totalGuesses: totalGuesses + 1, 
+        trump: null, 
+        kanye: null,
+        correctGuesses: answer === e.target.value ? correctGuesses + 1 : correctGuesses,
+        isCorrectTrump: answer === 'trump' ? true : false,
+        isCorrectKanye: answer === 'kanye' ? true : false
+      }, async () => {
+        try {
+          const resRandom = await getSingleQuote()
+          console.log(resRandom.data)
+          this.setState({ trump: resRandom.data.message, kanye: resRandom.data.quote, isCorrectTrump: false, isCorrectKanye: false })
+        } catch (err) {
+          console.log(err)
+        }
+      })
+    } else {
+      console.log(`${this.state.correctGuesses} / 10`)
+      console.log(this.state.totalGuesses)
+      console.log(this.state.score)
+      this.setState({ trump: null, kanye: null, score: `Congrats, you scored: ${this.state.correctGuesses} / 10` })
+    }
+  }
+
+  displayScore = () => {
+    this.setState({ trump: null, kanye: null, score: `Congrats, you scored: ${this.state.correctGuesses} / 10` })
+  }
+
+  endGame = () => {
+    return this.state.totalGuesses === 10
+  }
+
   render() {
     const { trump } = this.state
     const { kanye } = this.state
-    const { random } = this.state
+    const { score } = this.state
     return (
       <section>
         <div className="wrapper-game">
           <div className="game-image">
+            <img src={image} alt="logo" />
           </div>
           <div className="game-quote">
-            <h2>Trump...</h2>
-            { trump }
-            <h2>Kanye...</h2>
             { kanye }
+            { trump }
+            { score }
           </div>
           <div className="game-buttons">
-            <button>Trump</button>
-            <button>Kanye</button>
+            <div className={ `${this.state.score ? 'win-buttons' : ''}` }>
+              <button onClick={this.handleGuess} className={`${this.state.isCorrectTrump ? 'correct' : ''}`} value="trump">Trump</button>
+              <button onClick={this.handleGuess} className={`${this.state.isCorrectKanye ? 'correct' : ''}`} value="kanye">Kanye</button>
+            </div>
           </div>
           <div className="game-restart">
             <Link to={'/'}><button>Restart</button></Link>
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,43 +1,45 @@
 import axios from 'axios'
 
 // Base URL's
-const baseUrlTrump = 'https://tronalddump.io/random/quote'
+const baseUrlTrump = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random'
 const baseUrlKanye = 'https://api.kanye.rest'
+
 // This API always returns a random quote
-
-
 // Trump Requests
-export const getSingleTrumpQuote = () => {
-  return axios.get(`${baseUrlTrump}`)
-}
-
-
+// export const getSingleTrumpQuote = () => {
+//   return axios.get(`${baseUrlTrump}`)
+// }
 // Kanye Requests
-export const getSingleKanyeQuote = () => {
-  return axios.get(`${baseUrlKanye}`)
-}
+// export const getSingleKanyeQuote = () => {
+//   return axios.get(`${baseUrlKanye}`)
+// }
 
-// Function
-
-// (1) Write a function to randomize the boolean (true or false)
-
-function getRandom() {
+const getRandom = () => {
   return Math.random() < 0.5
 }
 
-// Random Function
-
-export const randomLine = (line) => {
-  return line
-}
-
-let baseUrl = ''
-
 export const getSingleQuote = () => {
   const isTrump = getRandom()
-  console.log(isTrump)
-  isTrump ? baseUrl = baseUrlTrump : baseUrl = baseUrlKanye
-  let line = ''
-  isTrump ? line = 'value' : line = 'quote'
-  return axios.get(`${baseUrl}`)
+  const baseUrl = isTrump ? baseUrlTrump : baseUrlKanye
+  return axios.get(baseUrl)
 }
+
+
+
+/// From Cheese
+
+
+
+// const getPayload = () => {
+//   const token = getToken()
+//   if (!token) return false
+//   const parts = token.split('.')
+//   if (parts.length < 3) return false
+//   return JSON.parse(window.atob(parts[1]))
+// }
+// export const isAuthenticated = () => {
+//   const payload = getPayload()
+//   if (!payload) return false
+//   const now = Math.round(Date.now() / 1000)
+//   return now < payload.exp
+// }
